3c49d00090644601ec52dddd1421879d21670642
Chandan Kumar <111229538+chandan771@users.noreply.github.com>
2023-07-31 07:59:40 +0000

    sync

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -14,9 +14,9 @@ import {
   StarIcon,
 } from "@heroicons/react/20/solid";
 
-import { 
-  ChevronLeftIcon, 
-  ChevronRightIcon,  
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
 } from "@heroicons/react/20/solid";
 import { Link } from "react-router-dom";
 
@@ -944,15 +944,11 @@ export default function ProductList() {
 
                   <div className="bg-white">
                     <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
-                      {/* <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                        Products
-                      </h2> */}
-
-                      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                      <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                         {products.map((product) => (
                           <Link to="/product-details">
-                            <div key={product.id} className="group relative">
-                              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                            <div key={product.id} className="group relative border-solid border-2 p-2 border-gray-200">
+                              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-60">
                                 <img
                                   src={product.thumbnail}
                                   alt={product.title}
@@ -967,19 +963,30 @@ export default function ProductList() {
                                         aria-hidden="true"
                                         className="absolute inset-0"
                                       >
-                                      {product.title}
+                                        {product.title}
                                       </span>
                                     </a>
                                   </h3>
-                                  <p className="mt-1 text-sm text-gray-500">
-                                    <StarIcon className="w-5 h-6 inline" />
-                                    <span className="align-bottom">{product.rating}</span>
-                                    
+                                  <p className="flex align-middle gap-2 mt-1 text-sm text-gray-500">
+                                    <StarIcon className="w-5 h-5 inline" />
+                                    <span className="">{product.rating}</span>
+
                                   </p>
                                 </div>
-                                <p className="text-sm font-medium text-gray-900">
-                                   ${product.price}
-                                </p>
+
+                                <div>
+                                  <p className="text-sm block font-medium text-gray-900">
+                                    $
+                                    {Math.round(
+                                      product.price * (1 - product.discountPercentage / 100)
+                                    )}
+                                  </p>
+
+                                  <p className="text-sm block line-through font-medium text-gray-400">
+                                    ${product.price}
+                                  </p>
+                                  
+                                </div>
                               </div>
                             </div>
                           </Link>
